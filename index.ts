9c25039f23b5e08291e1500183ad2723721b9a61
Davi Siepmann <davicoradini@gmail.com>
2020-08-08 10:49:21 -0400

    change

--- index.ts
+++ index.ts
@@ -3,4 +3,9 @@ import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const var1 = { user: { id: 1 } }
+
+const var2 = var1?.user.id;
+
+appDiv.innerHTML = `<h1>TypeScript Starter: ${var2}</h1>`;
